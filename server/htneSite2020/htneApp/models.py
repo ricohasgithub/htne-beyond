5f2d7bc131618176c9e5a880869a4e247f507531
Rahul Sunil <rahulsunil2@gmail.com>
2020-09-05 10:05:23 +0530

    Profile

--- server/htneSite2020/htneApp/models.py
+++ server/htneSite2020/htneApp/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school = models.CharField(max_length=125)
+    grad_year = models.DateField()
+    gender = models.CharField(max_length=1, choices=(("M", "Male"), ("F", "Female")))
+    designation = models.CharField(max_length=25)
+    desc = models.TextField()
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.profile.save()
